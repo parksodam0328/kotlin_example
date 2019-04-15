efa5f1357d3e7b88414f5ad15c01da00649f0760
parksodam0328 <parksodam0228@e-mirim.hs.kr>
2019-04-15 17:13:53 +0900

    button click event

--- Calculator/app/src/main/java/com/springsoft/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/springsoft/calculator/MainActivity.kt
@@ -2,11 +2,44 @@ package com.springsoft.calculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var tvNumber : TextView
+    private var isStart = true
+    private var isNumber = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvNumber = findViewById(R.id.testView)
+    }
+
+    fun onNumber(view : View){
+        if(isStart) {
+            tvNumber.text = (view as Button).text
+            isStart = false
+            isNumber = true
+        }else{
+            tvNumber.append((view as Button).text)
+            isNumber = true
+        }
+    }
+
+    fun onCalculator(view: View){
+        if(isNumber){
+            tvNumber.append((view as Button).text)
+            isNumber = false
+        }else{
+            Toast.makeText(applicationContext, "연속으로 연산자를 쓸 수 없습니다.", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    fun onClear(view: View){
+        tvNumber.text = ""
     }
 }
