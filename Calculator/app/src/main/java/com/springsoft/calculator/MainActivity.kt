0e0f6210434e01502a089da0f8e5d1194e15d051
parksodam0328 <parksodam0228@e-mirim.hs.kr>
2019-04-15 18:17:41 +0900

    add calculator

--- Calculator/app/src/main/java/com/springsoft/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/springsoft/calculator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.springsoft.calculator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -10,36 +11,67 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var tvNumber : TextView
-    private var isStart = true
-    private var isNumber = false
+    private var isFirst = true
+
+    private var firstNumber : Int = 0
+    private var secondNumber : Int = 0
+
+    private var result : Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tvNumber = findViewById(R.id.testView)
     }
 
-    fun onNumber(view : View){
-        if(isStart) {
-            tvNumber.text = (view as Button).text
-            isStart = false
-            isNumber = true
-        }else{
-            tvNumber.append((view as Button).text)
-            isNumber = true
+    fun onCalculator(view: View){
+        when(view.id){
+            R.id.plus -> {
+                result = firstNumber + secondNumber
+                tvNumber.append((view as Button).text)
+                Log.e("MainActivity",result.toString())
+            }
+            R.id.minus -> {
+                result = firstNumber - secondNumber
+                tvNumber.append((view as Button).text)
+            }
+            R.id.divide -> {
+                if(firstNumber != 0 && secondNumber != 0) {
+                    result = firstNumber / secondNumber
+                    tvNumber.append((view as Button).text)
+                }
+                else{
+                    Toast.makeText(applicationContext,"나누기 error", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                result = firstNumber * secondNumber
+                tvNumber.append((view as Button).text)
+            }
         }
     }
 
-    fun onCalculator(view: View){
-        if(isNumber){
+    fun onNumber(view: View){
+        if(isFirst){
             tvNumber.append((view as Button).text)
-            isNumber = false
-        }else{
-            Toast.makeText(applicationContext, "연속으로 연산자를 쓸 수 없습니다.", Toast.LENGTH_SHORT).show()
+            firstNumber = ((view as Button).text).toString().toInt()
+            Log.e("MainActivity",firstNumber.toString())
+            isFirst = false
+        } else{
+            tvNumber.append((view as Button).text)
+            secondNumber = ((view as Button).text).toString().toInt()
+            Log.e("MainActivity",secondNumber.toString())
+            isFirst = true
         }
+    }
 
+    fun onResult(view: View){
+        tvNumber.text = result.toString()
     }
 
     fun onClear(view: View){
         tvNumber.text = ""
+        firstNumber = 0
+        secondNumber = 0
     }
 }
